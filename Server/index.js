276f40300f880824b131924ee08ef65f33de3ae4
Michael Teeuw <michael@xonaymedia.nl>
2018-12-10 12:00:46 +0100

    Add Server

---
+++ Server/index.js
@@ -0,0 +1,53 @@
+var config = require('config.js')
+var mqtt = require('mqtt')
+var axios = require('axios')
+var client  = mqtt.connect(config.mqtt.server ,{ username: config.mqtt.username, password: config.mqtt.password })
+var playlist = require('./playlist.js')
+const baseUrl = config.sonos.baseUrl + '/' + config.sonos.room + '/'
+const CancelToken = axios.CancelToken;
+var cancelToken = null
+
+console.log(playlist)
+
+client.on('connect', function () {
+  client.subscribe('musicubes/#', function (err) {
+    if (err) {
+      console.log(err)
+    }
+    console.log('Ready to receive a MusiCube')
+  })
+})
+client.on('message', function (topic, message) {
+  const uri = getMusiCube(message)
+  if (!uri) return console.log('Unknown MusiCube: ' + message)
+  requestSonosUri(uri)
+})
+
+function getMusiCube(identifier) {
+  if (identifier == '00:00:00:00') return 'pause'
+  if (identifier in playlist) return playlist[identifier]
+
+  return false
+}
+
+function requestSonosUri(uri) {
+  console.log('Requesting: ' + uri)
+
+  if (cancelToken) {
+    cancelToken.cancel();
+  }
+
+  cancelToken = CancelToken.source();
+  axios.get(baseUrl + uri,{
+    cancelToken: cancelToken.token
+  })
+    .then(result => {
+      console.log('Request confirmed: ' + uri)
+    })
+    .catch(e => {
+      if (axios.isCancel(e)) {
+        return console.log('Request canceled: ', uri);
+      }
+      console.log(uri + ' is not a valid endpoint.')
+    })
+}
---
+++ Server/playlist.js
@@ -0,0 +1,15 @@
+const playlist = {
+    '04:1e:3d:ba': 'favorite/538',
+    '04:12:3d:ba': 'favorite/Koffee',
+    '04:1a:3d:ba': 'favorite/Lekkere%20nummertjes',
+    '04:0e:3d:ba': 'spotify/now/spotify:user:defected.records:playlist:08M2kTqmlPNHygrNfrZJuQ',
+    '04:16:3d:ba': 'spotify/now/spotify:user:spotify:playlist:37i9dQZEVXcWW8vANM3Ai8',
+
+    '04:31:3c:ba': 'playlist/kinderliedjes', //Kinderliedjes
+    '04:25:3c:ba': 'favorite/BLUE MARLIN IBIZA RADIO', //Blue Marlin Radio
+    '04:22:3d:ba': 'favorite/Ibiza Global Radio', //Ibiza Global Radio
+    '04:29:3c:ba': 'spotify/now/spotify:user:21ifzjxggdajdpc4w6iqxslkq:playlist:4hfe8itOfwwijAHHOlI6pp', //Top 40
+
+}
+
+module.exports = playlist
